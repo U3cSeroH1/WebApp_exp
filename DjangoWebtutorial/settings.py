bcf575462a7e0d47ffe9680560a7207f21285955
U3cSeroH1 <unkio0602@gmail.com>
2018-11-12 21:21:30 +0900

    1112009

--- DjangoWebtutorial/settings.py
+++ DjangoWebtutorial/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
 
     'register.apps.RegisterConfig',
 
-    #'django.contrib.admin',
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
