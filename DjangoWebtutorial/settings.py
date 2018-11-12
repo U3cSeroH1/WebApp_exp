20d81478ce97d7d4320103e928e23fb4f54cdaf3
U3cSeroH1 <unkio0602@gmail.com>
2018-11-12 22:33:08 +0900

    1112018

--- DjangoWebtutorial/settings.py
+++ DjangoWebtutorial/settings.py
@@ -137,7 +137,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # カスタムユーザーを使う
-AUTH_USER_MODEL = 'register.User'
+#AUTH_USER_MODEL = 'register.User'
 
 # ログインページと、直接ログインページへ行った後のリダイレクトページ
 LOGIN_URL = 'register:login'
