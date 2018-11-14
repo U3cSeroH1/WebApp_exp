a93952a3adde13600b43710cf9f21e639e4d7d53
U3cSeroH1 <unkio0602@gmail.com>
2018-11-15 07:26:24 +0900

    11150001

--- DjangoWebtutorial/settings.py
+++ DjangoWebtutorial/settings.py
@@ -14,6 +14,8 @@ import os
 import posixpath
 import django
 
+import dj_database_url
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -163,3 +165,8 @@ except ImportError:
 if not DEBUG:
     import django_heroku
     django_heroku.settings(locals())
+
+
+db_from_env = dj_database_url.config(conn_max_age=400)
+DATABASES['default'].update(db_from_env)
+
