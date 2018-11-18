42fa21205ce75ae9ce273297efbbb92b7c397bf6
U3cSeroH1 <unkio0602@gmail.com>
2018-11-18 20:17:36 +0900

    11180f15

--- register/forms.py
+++ register/forms.py
@@ -15,7 +15,7 @@ class LoginForm(AuthenticationForm):
         super().__init__(*args, **kwargs)
         for field in self.fields.values():
             field.widget.attrs['class'] = 'form-control'
-            #field.widget.attrs['placeholder'] = field.label  # placeholderにフィールドのラベルを入れる
+            field.widget.attrs['placeholder'] = field.label  # placeholderにフィールドのラベルを入れる
 
 
 class UserCreateForm(UserCreationForm):
