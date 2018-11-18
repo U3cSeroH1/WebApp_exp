1461e18fd01c5fcb73a880663ecf388fe46bea85
U3cSeroH1 <unkio0602@gmail.com>
2018-11-18 16:31:39 +0900

    111800f2

--- DjangoWebtutorial/settings.py
+++ DjangoWebtutorial/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
 
 
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -136,7 +137,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 STATICFILES_DIRS = (
