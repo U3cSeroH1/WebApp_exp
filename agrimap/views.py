d17ed9d575f29e01c023475e5c74fc0a5a37f2de
U3cSeroH1 <unkio0602@gmail.com>
2018-11-26 15:15:04 +0900

    13440041

--- agrimap/views.py
+++ agrimap/views.py
@@ -25,7 +25,7 @@ from register.views import OnlyYouMixin
 
 
 
-class DetailView(OnlyYouMixin, generic.TemplateView):
+class DetailView(generic.TemplateView):
     """ユーザーの詳細ページ"""
     model = User
     template_name = 'agrimap/detail.html'  # デフォルトユーザーを使う場合に備え、きちんとtemplate名を書く
