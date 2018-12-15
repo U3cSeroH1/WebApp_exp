4bded4fd41ae10daa0e11c0477aea6e3aa7f93aa
U3cSeroH1 <unkio0602@gmail.com>
2018-12-15 16:18:57 +0900

    12150001

--- agrimap/models.py
+++ agrimap/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class latlng(models.Model):
-    # id = AutoField(primary_key=True)  # �����I�ɒǉ������̂Œ�`�s�v
+    # id = AutoField(primary_key=True)
 
-    lat = models.lat(max_length=255, unique=True)
-    lng = models.lng(max_length=255, unique=True)
+    #lat = models.lat(max_length=255, unique=True)
+    #lng = models.lng(max_length=255, unique=True)
