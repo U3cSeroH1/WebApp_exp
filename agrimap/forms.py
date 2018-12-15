89df0de958e200c44674107ecc9672e829de02fe
U3cSeroH1 <unkio0602@gmail.com>
2018-12-15 23:15:51 +0900

    12150118

---
+++ agrimap/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+
+class latlngForm(forms.Form):
+    """ログインフォーム"""
+
+
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
+            field.widget.attrs['placeholder'] = field.label  # placeholderにフィールドのラベルを入れる
