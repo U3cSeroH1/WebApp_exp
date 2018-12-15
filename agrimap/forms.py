02b6e0e83fcec566109b629ff33aa2ce059c6b81
U3cSeroH1 <unkio0602@gmail.com>
2018-12-16 07:54:13 +0900

    12150152

--- agrimap/forms.py
+++ agrimap/forms.py
@@ -5,10 +5,16 @@ from .models import latlng
 class latlngForm(forms.ModelForm):
     """ログインフォーム"""
 
+
+
     class Meta:
 
         model = latlng
 
         fields = ('lat', 'lng')
 
-   
+
+class ProfileForm(forms.Form):
+
+    name = forms.CharField()
+    age = forms.IntegerField() 
--- agrimap/views.py
+++ agrimap/views.py
@@ -54,7 +54,7 @@ class DetailView(OnlyYouMixin, generic.DetailView):
     """ユーザーの詳細ページ"""
     model = User
 
-    form_class = latlngForm
+    #form_class = latlngForm
 
 
     template_name = 'agrimap/detail.html'  # デフォルトユーザーを使う場合に備え、きちんとtemplate名を書く
