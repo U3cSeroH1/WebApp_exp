82533d49ad67559618231ba188f859ce1c4de314
U3cSeroH1 <unkio0602@gmail.com>
2018-12-16 07:59:25 +0900

    12160001

--- agrimap/views.py
+++ agrimap/views.py
@@ -56,6 +56,7 @@ class DetailView(OnlyYouMixin, generic.DetailView):
 
     #form_class = latlngForm
 
+    form_class = ProfileForm
 
     template_name = 'agrimap/detail.html'  # デフォルトユーザーを使う場合に備え、きちんとtemplate名を書く
 
