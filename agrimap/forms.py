f881323f8acf8990d5cf42753c018a0ab543a7e7
U3cSeroH1 <unkio0602@gmail.com>
2018-12-16 00:17:27 +0900

    12150127

--- agrimap/forms.py
+++ agrimap/forms.py
@@ -11,8 +11,3 @@ class latlngForm(forms.ModelForm):
 
         fields = ('lat', 'lng')
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for field in self.fields.values():
-            field.widget.attrs['class'] = 'form-control'
-            field.widget.attrs['placeholder'] = field.label  # placeholderにフィールドのラベルを入れる
