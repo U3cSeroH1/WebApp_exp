a680381e7fcb665d9896e299130e96769cb17c97
U3cSeroH1 <unkio0602@gmail.com>
2018-12-16 09:04:18 +0900

    12160012

--- agrimap/models.py
+++ agrimap/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
+from comment.models import Post, Comment, Reply
+
 class latlng(models.Model):
     # id = AutoField(primary_key=True)
 
-    lat = models.CharField(unique=True)
-    lng = models.CharField(unique=True)
+    lat = models.IntegerField(unique=True)
+    lng = models.IntegerField(unique=True)
 
 
 
