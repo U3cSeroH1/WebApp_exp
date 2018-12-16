6cf736adde843441e75e66bea142d2f61809566c
U3cSeroH1 <unkio0602@gmail.com>
2018-12-16 21:22:24 +0900

    12160606

--- comment/admin.py
+++ comment/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from comment.models import Reply, Post, Comment
 
 # Register your models here.
+admin.site.register(Reply)
+admin.site.register(Post)
+admin.site.register(Comment)
