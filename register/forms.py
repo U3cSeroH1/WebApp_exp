2c875fc2b328c973f9b42971ffaacb30909af013
U3cSeroH1 <unkio0602@gmail.com>
2018-12-17 13:53:23 +0900

    '12170001'

--- register/forms.py
+++ register/forms.py
@@ -32,6 +32,7 @@ class UserCreateForm(UserCreationForm):
         super().__init__(*args, **kwargs)
         for field in self.fields.values():
             field.widget.attrs['class'] = 'form-control'
+            field.widget.attrs['placeholder'] = 'テキスト'
 
 
 class UserUpdateForm(forms.ModelForm):
